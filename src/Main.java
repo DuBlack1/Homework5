32cebd1767c3e65ab37c8980e99c6cb5b1576f3a
DuBlack1 <asya.schorikowa@yandex.ru>
2022-07-25 11:09:32 +0500

    Домашка выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,76 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+
+        double[] arr2 = {1.57, 7.654, 9.986};
+
+        char[] arr3 = {14, 15, 5, 16};
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (i!=arr1.length - 1) {
+                System.out.print(arr1[i] + ", ");
+            }
+            else {
+                System.out.print(arr1[i]);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < arr2.length; i++) {
+            if (i!=arr2.length - 1) {
+                System.out.print(arr2[i] + ", ");
+            }
+            else {
+                System.out.print(arr2[i]);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < arr3.length; i++) {
+            if (i!=arr3.length - 1) {
+                System.out.print(arr3[i] + ", ");
+            }
+            else {
+                System.out.print(arr3[i]);
+            }
+        }
+        System.out.println();
+        for (int i = arr1.length - 1; i < arr1.length && i > -1; i--) {
+            if (i != 0) {
+                System.out.print(arr1[i] + ", ");
+            }
+            else {
+                System.out.print(arr1[i]);
+            }
+        }
+        System.out.println();
+        for (int i = arr2.length - 1; i < arr2.length && i > -1; i--) {
+            if (i != 0) {
+                System.out.print(arr2[i] + ", ");
+            }
+            else {
+                System.out.print(arr2[i]);
+            }
+        }
+        System.out.println();
+        for (int i = arr3.length - 1; i < arr3.length && i > -1; i--) {
+            if (i != 0) {
+                System.out.print(arr3[i] + ", ");
+            }
+            else {
+                System.out.print(arr3[i]);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i]%2 != 0) {
+                arr1[i] += 1;
+            }
+        }
     }
 }
